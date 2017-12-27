2ca52f9802601a7e3e579684ee39d55e37615c34
Paweł Wieczorek <pawiecz@ep1c.win>
2017-12-27 14:56:51 +0100

    Add index action with tests

--- handlers_test.go
+++ handlers_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 
+	. "github.com/Benjamintf1/ExpandedUnmarshalledMatchers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -19,11 +20,24 @@ func newRequestRecorder(req *http.Request, method string, strPath string, fnHand
 }
 
 var _ = Describe("Main", func() {
+	BeforeSuite(func() {
+		module["I1"] = &Relay{ID: "I1", Pin: 17, State: false}
+		module["I2"] = &Relay{ID: "I2", Pin: 27, State: true}
+	})
+
 	It("should report for duty on the default route", func() {
 		expectedResponse := "relayctl reporting for duty!\n"
 		req := httptest.NewRequest("GET", "/", nil)
 		rr := newRequestRecorder(req, "GET", "/", Index)
 		Expect(rr.Code).To(Equal(http.StatusOK))
 		Expect(rr.Body.String()).To(Equal(expectedResponse))
+	})
+
+	It("should list available relays", func() {
+		expectedResponse := "{\"meta\":null,\"data\":[{\"id\":\"I1\",\"pin\":17,\"state\":false},{\"id\":\"I2\",\"pin\":27,\"state\":true}]}\n"
+		req := httptest.NewRequest("GET", "/relays", nil)
+		rr := newRequestRecorder(req, "GET", "/relays", RelayIndex)
+		Expect(rr.Code).To(Equal(http.StatusOK))
+		Expect(rr.Body.String()).To(MatchUnorderedJSON(expectedResponse))
 	})
 })
--- relayctl.go
+++ relayctl.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,9 +13,39 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "relayctl reporting for duty!\n")
 }
 
+// Relay represents a single switch on controlled module.
+type Relay struct {
+	ID    string `json:"id"`
+	Pin   int    `json:"pin"`
+	State bool   `json:"state"`
+}
+
+type JsonResponse struct {
+	Meta interface{} `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+// module stores state of relays with their IDs as keys.
+var module = make(map[string]*Relay)
+
+// RelayIndex handles the relays index action (GET /relays).
+func RelayIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var relays []*Relay
+	for _, relay := range module {
+		relays = append(relays, relay)
+	}
+	response := &JsonResponse{Data: &relays}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.GET("/relays", RelayIndex)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
